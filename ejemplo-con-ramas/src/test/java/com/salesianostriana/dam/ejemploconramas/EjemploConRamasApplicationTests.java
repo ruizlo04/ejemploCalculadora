917051bedeb182f1048d2c12dececb6e6b5de1d6
Carlos <ruiz.locar23@triana.salesianos.edu>
2024-04-22 13:14:53 +0200

    Primer commit

---
+++ ejemplo-con-ramas/src/test/java/com/salesianostriana/dam/ejemploconramas/EjemploConRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploconramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploConRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
